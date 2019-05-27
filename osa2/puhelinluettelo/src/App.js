a8bdb3b30ad73ca3f035f499776df2cee99faa8a
Lasse Säde <lasse.sade@gmail.com>
2019-05-27 19:28:17 +0300

    puhelin

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Person from './components/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [persons, setPersons] = useState([
+        { 
+            name: 'Arto Hellas',
+            number: '000-00000'
+        }
+    ])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+    const addName = (event) => {
+        event.preventDefault()
+
+        const nameObject = {
+            name: newName,
+            number: newNumber
+        }
+
+        const samoja = persons.filter(person => person.name === newName)
+    
+        var add = (samoja.length > 0)?false:true;
+        if (add) {
+            setPersons(persons.concat(nameObject))
+            setNewName('')
+            setNewNumber('')
+        }
+        else  
+            alert(`${newName} on jo luettelossa`)
+    }
+    const names = persons.map(
+        person => <Person key={person.name} person={person} />
+    )
+    return (
+        <div>
+            <h2>Puhelinluettelo</h2>
+            <form onSubmit={addName}>
+                <div>
+                    nimi: <input
+                        value={newName}
+                        onChange={handleNameChange}
+                    /><br />
+                    numero: <input
+                        value={newNumber}
+                        onChange={handleNumberChange}
+                    />
+                </div>
+                <div>
+                    <button type="submit">lisää</button>
+                </div>
+            </form>
+            <h2>Numerot</h2>
+            {names}
+        </div>
+    )
+
 }
 
-export default App;
+export default App
---
+++ osa2/puhelinluettelo/src/components/Person.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Person = ({ person }) => {
+    return (
+      <li>{person.name} - {person.number}</li>
+    )
+  }
+
+  export default Person
