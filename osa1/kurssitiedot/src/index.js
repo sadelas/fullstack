f43733dd00ac011d037d9c592cc1534a68656ee1
Lasse Säde <lasse.sade@gmail.com>
2019-05-27 18:34:32 +0300

    initial commit

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,45 +3,57 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
     return (
-        <h1>{props.nimi}</h1>
+        <h1>{props.course}</h1>
     )
 }
 
 const Content = (props) => {
+    
     return (
         <div>
-        <Part part={props.part1} ex={props.ex1} />
-        <Part part={props.part2} ex={props.ex2} />
-        <Part part={props.part3} ex={props.ex3} />
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
         </div>
     )
 }
 
 const Part = (props) => {
+    
     return (
-        <p>{props.part} {props.ex}</p>
+        <p>{props.part.name} {props.part.exercises}</p>
     )
 }
 
 const Total = (props) => {
+    
     return (
-        <p>yhteensä {props.yht} tehtävää</p> 
+        <p>yhteensä {props.parts[0].exercises  + props.parts[1].exercises + props.parts[2].exercises} tehtävää</p> 
     )
 }
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
-
+    const course = {
+      name: 'Half Stack -sovelluskehitys',
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14
+        }
+      ]
+    }
   return (
     <div>
-      <Header nimi={course} />
-      <Content part1={part1} ex1={exercises1} part2={part2} ex2={exercises2} part3={part3} ex3={exercises3} />
-      <Total yht={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,12 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Statistic = (props) => {
+  return (
+    
+      <tr>
+      <td>{props.text}</td><td>{props.value}{props.pre}</td>
+      </tr>
+      
+  )
+}
+const Statistics = (props) => {
+  if (props.total > 0) {
+    return (
+     <div>
+        <h1>Statistiikka</h1>
+        <table>
+          <tbody>
+              <Statistic text="hyvä" value={props.good}/>
+              <Statistic text="neutraali" value={props.neutral}/>
+              <Statistic text="huono" value={props.bad}/>
+              <Statistic text="yhteensä" value={props.total}/>
+              <Statistic text="keskiarvo" value={props.avg}/>
+              <Statistic text="positiivisia" value={props.positive} pre=" %"/>
+          </tbody>
+        </table>
+        
+        
+       
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>Statistiikka</h1>
+      <p>Ei yhtään palautetta annettu</p>
+    </div>
+    )
+  
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+   
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+    
+  }
+  const handleBadClick = () => {
+    
+    setBad(bad + 1);
+    setTotal(total + 1);
+    
+  }
+  const calcAvg = () => {
+    setAvg( (good - bad) / total)
+    
+  }
+  const calcPos = () => [
+    setPositive(good / total * 100)
+  ]
+  const Button = ({ handleClick,text }) => {
+    calcAvg()
+    calcPos()
+    return (
+    <button onClick={handleClick}>
+        {text}  
+    </button>
+  
+  )}
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAvg] = useState(0);
+  const [positive, setPositive] = useState(0);
+  return (
+    <div>
+        <h1>Anna palutetta</h1>
+        <Button handleClick={handleGoodClick} text='hyvä' />
+        <Button handleClick={handleNeutralClick}  text='neutraali' />
+        <Button handleClick={handleBadClick} text='huono' />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} avg={avg} positive={positive} />
+      
+      
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
